443c8dd16ac4a35a675961e76eb239cdd3ffda81
Joey Ganley <jganley@g.ucla.edu>
2020-12-10 14:24:59 -0800

    accidently copied something in there

--- elasticsearchScripts/initIndex.sh
+++ elasticsearchScripts/initIndex.sh
@@ -1,28 +1,4 @@
 #!/bin/bash
 
 # Create and index the documents using the default standard analyzer
-
-'
-{
-   "mappings": {
-      "doc": {
-         "properties": {
-            "parent": {
-               "type": "nested",
-               "properties": {
-                  "label": {
-                     "type": "string"
-                  },
-                  "name": {
-                     "type": "string"
-                  }
-               }
-            }
-         }
-      }
-   }
-}
-'
-
-
 curl -s -H "Content-Type: application/x-ndjson" -XPOST 'localhost:9200/dexter/frame/_bulk?pretty' --data-binary @../elasticsearchInput/elasticsearch_bulk_input.txt
